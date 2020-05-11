18f482c0afc52ea3c1ec07cf7bfd077d2b6618d2
ajschwieterman <50152740+ajschwieterman@users.noreply.github.com>
2020-05-11 12:28:20 -0400

    Add files via upload

--- EquinoxClock.ino
+++ EquinoxClock.ino
@@ -9,8 +9,8 @@
 #include <TimeLib.h>
 
 #ifndef STASSID
-#define STASSID ""
-#define STAPSK  ""
+#define STASSID "Tangy Elephants"
+#define STAPSK  "Tintlab3A"
 #define BUTTON_PIN  12
 #define CYCLONE_PIN 13  
 #define PHOTOCELL_PIN A0
